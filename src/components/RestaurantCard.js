ed93513c2fd31f6ab4c3787143c6de9b3c6c698d
Sagar Pujar <sagar.pujar@iRely.com>
2026-08-19 07:09:10 +0530

    Episode 10 - Tailwind CSS and Implementing

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -13,7 +13,7 @@ const RestaurantCard = (props) => {
     : "https://dineout-media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_600,h_400/DINEOUT_ALL_RESTAURANTS/IMAGES/RESTAURANT_IMAGE_SERVICE/2025/8/30/0c394307-621b-4499-8d2c-19c88a221cf5_image01900ee3f119343d2b35697131496aefc.JPG";
 
   return (
-    <div className="card m-4 p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 w-[300px] bg-gray-50 hover:bg-gray-100">
+    <div className="card m-4 p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 bg-gray-50 hover:bg-gray-100">
       <div className="card-content">
         <img
           className="card-img rounded-lg"
